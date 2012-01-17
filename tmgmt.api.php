a1e901909da6a3e2d06d8d08d524952955c788dc
Sascha Grossenbacher <saschagros@gmail.com>
2012-01-17 21:16:13 +0100

    Add hook documentation and documentation topics, group functions into topics.

--- tmgmt.api.php
+++ tmgmt.api.php
@@ -10,7 +10,47 @@
  * @{
  */
 
+/**
+ * Provide information about source plugins.
+ */
+function hook_tmgmt_source_plugin_info() {
+  return array(
+    'test_source' => array(
+      'label' => t('Test source'),
+      'description' => t('Simple source for testing purposes.'),
+      'controller class' => 'TMGMTTestSourcePluginController',
+    ),
+  );
+}
 
+/**
+ * Alter source plugins information.
+ *
+ * @param $info
+ */
+function hook_tmgmt_source_plugin_info_alter(&$info) {
+
+}
+
+/**
+ * Provide information about translator plugins.
+ */
+function hook_tmgmt_translator_plugin_info() {
+  return array(
+    'test_translator' => array(
+      'label' => t('Test translator'),
+      'description' => t('Simple translator for testing purposes.'),
+      'controller class' => 'TMGMTTestTranslatorPluginController',
+    ),
+  );
+}
+
+/**
+ * Alter information about translator plugins.
+ */
+function hook_tmgmt_translator_plugin_info_alter(&$info) {
+
+}
 
 /**
  * @} End of "addtogroup hooks".
