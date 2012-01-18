363ccdf012e0d9e80a4ff028b8c0d39c423c0d4d
Sascha Grossenbacher <saschagros@gmail.com>
2012-01-18 09:02:39 +0100

    Really fixed code tags.

--- tmgmt.api.php
+++ tmgmt.api.php
@@ -70,7 +70,7 @@ function hook_tmgmt_translator_plugin_info_alter(&$info) {
  * and the type and id, which the source plugin then uses to identify it later
  * on.
  *
- * <code>
+ * @code
  * $job = tmgmt_job_create('en', $target_language);
  * // Job needs to be saved first.
  * // @todo: Fix this.
@@ -80,18 +80,18 @@ function hook_tmgmt_translator_plugin_info_alter(&$info) {
  *   $item = tmgmt_job_item_create('test_source', 'test', $i);
  *   tmgmt_job_add_item($job, $item);
  * }
- * </endcode>
+ * @endcode
  *
  * Once a job has been created, it can be assigned to a translator plugin, which
  * is the service that is going to do the translation.
  *
- * <code>
+ * @code
  * $job->translator = 'test_translator';
  * // Translator specific settings.
  * $job->translator_context = array(
  *   'prioritoy' => 5,
  * );
- * </endcode>
+ * @endcode
  *
  * // Get the translator plugin and request a translation.
  * $translator = $job->getTranslatorPlugin();
@@ -106,13 +106,13 @@ function hook_tmgmt_translator_plugin_info_alter(&$info) {
  * The translation can now be reviewed, accepted and the source plugins be told
  * // to save the translation.
  *
- * <code>
+ * @code
  * // @todo: Avoid direct state assignements.
  * $job->state = TMGMTJob::STATE_ACCEPTED;
  * $job->save();
  * tmgmt_job_save_translations($job);
  * // @todo: Does this need a new state?
- * </endcode>
+ * @endcode
  */
 
 /**
