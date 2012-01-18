b38305595d707d3028b974c5c708c674c01c450f
Sascha Grossenbacher <saschagros@gmail.com>
2012-01-18 08:53:06 +0100

    Added first documentation attempt for translation jobs.

--- tmgmt.api.php
+++ tmgmt.api.php
@@ -59,7 +59,59 @@ function hook_tmgmt_translator_plugin_info_alter(&$info) {
 /**
  * @defgroup job Translation Jobs
  *
- * @todo
+ * A single task to translate something into a given language using a @link
+ * translator Translator @endlink.
+ *
+ * Attached to these jobs are job items, which specifiy which @link source
+ * Sources @endlink are to be translated.
+ *
+ * To create a new translation job, first create a job and then assign items to
+ * each. Each item needs to specificy the source plugin that should be used
+ * and the type and id, which the source plugin then uses to identify it later
+ * on.
+ *
+ * <?php
+ * $job = tmgmt_job_create('en', $target_language);
+ * // Job needs to be saved first.
+ * // @todo: Fix this.
+ * $job->save();
+ *
+ * for ($i = 1; $i < 3; $i++) {
+ *   $item = tmgmt_job_item_create('test_source', 'test', $i);
+ *   tmgmt_job_add_item($job, $item);
+ * }
+ * ?>
+ *
+ * Once a job has been created, it can be assigned to a translator plugin, which
+ * is the service that is going to do the translation.
+ *
+ * <?php
+ * $job->translator = 'test_translator';
+ * // Translator specific settings.
+ * $job->translator_context = array(
+ *   'prioritoy' => 5,
+ * );
+ *
+ * // Get the translator plugin and request a translation.
+ * $translator = $job->getTranslatorPlugin();
+ * $translator->requestTranslation($job);
+ * ?>
+ *
+ * The translation plugin will then request the text from the source plugin.
+ * Depending on the plugin, the text might be sent to an external service
+ * or assign it to a local user or team of users. At some point, a translation
+ * will be returned and saved in the translation.
+ *
+ * The translation can now be reviewed, accepted and the source plugins be told
+ * // to save the translation.
+ *
+ * <?php
+ * // @todo: Avoid direct state assignements.
+ * $job->state = TMGMTJob::STATE_ACCEPTED;
+ * $job->save();
+ * tmgmt_job_save_translations($job);
+ * // @todo: Does this need a new state?
+ * ?>
  */
 
 /**
