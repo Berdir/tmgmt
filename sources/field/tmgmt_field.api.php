e314db949ba34e07d1611b76f48c544016f52d79
Sascha Grossenbacher <saschagros@gmail.com>
2012-12-23 12:08:00 +0100

    refactored

--- sources/field/tmgmt_field.api.php
+++ sources/field/tmgmt_field.api.php
@@ -8,6 +8,19 @@
 /**
  * Extract translatable text elements from a field.
  *
+ * @param $entity_type
+ *   The type of $entity.
+ * @param $entity
+ *   The entity being extracted.
+ * @param $field
+ *   The field structure.
+ * @param $instance
+ *   The field instance.
+ * @param $langcode
+ *   The language associated with $items.
+ * @param $items
+ *   Array of values for this field.
+ *
  * @return
  *   An array of translatable text elements, keyed by the schema name of the
  *   field.
@@ -16,6 +29,6 @@
  *
  * @ingroup tmgmt_source
  */
-function hook_tmgmt_source_translation_structure($field_name, $entity_type, $entity, $field_instance) {
+function hook_tmgmt_source_translation_structure($entity_type, $entity, $field, $instance, $langcode, $items) {
 
 }
