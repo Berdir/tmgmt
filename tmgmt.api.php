76f964ea57f2d048a2510b112840bbf862e78b2c
Sascha Grossenbacher <saschagros@gmail.com>
2013-05-30 16:49:34 +0200

    applied patch

--- tmgmt.api.php
+++ tmgmt.api.php
@@ -38,6 +38,33 @@ function hook_tmgmt_source_plugin_info_alter(&$info) {
 }
 
 /**
+ * Return a list of suggested sources for job items.
+ *
+ * @param array $items
+ *   An array with TMGMTJobItem objects which must be checked for suggested
+ *   translations.
+ *   - TMGMTJobItem A JobItem to check for suggestions.
+ *   - ...
+ * @param TMGMTJob $job
+ *   The current translation job to check for additional translation items.
+ *
+ * @return array
+ *   An array with all additional translation suggestions.
+ *   - job_item: A TMGMTJobItem instance.
+ *   - referenced: A string which indicates where this suggestion comes from.
+ *   - from_job: The main TMGMTJob-ID which suggests this translation.
+ */
+function hook_tmgmt_source_suggestions(array $items, TMGMTJob $job) {
+  return array(
+    array(
+      'job_item' => tmgmt_job_item_create('entity', 'node', 0),
+      'referenced' => t('Referenced @type of field @label', array('@type' => 'entity', '@label' => 'label')),
+      'from_job' => $job->tjid,
+    )
+  );
+}
+
+/**
  * @} End of "addtogroup tmgmt_source".
  */
 
