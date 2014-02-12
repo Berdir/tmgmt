1584d3a01930a8c828ba6a4f30cc5e6a7be0de06
divamys <divamys@128652.no-reply.drupal.org>
2014-02-12 16:38:19 +0100

    Issue #1882108 by blueminds, miro_dietiker: Mask HTML markup in XLIFF.

--- translators/file/tmgmt_file.api.php
+++ translators/file/tmgmt_file.api.php
@@ -32,6 +32,22 @@ function hook_tmgmt_file_format_plugin_info() {
 }
 
 /**
+ * Provide information about available text processors.
+ *
+ * @return array
+ *   An array of available text processor definitions. The key is the text
+ *   processor name.
+ */
+function hook_tmgmt_file_text_processor_plugin_info() {
+  return array(
+    'mask_html_for_xliff' => array(
+      'label' => t('Escape HTML'),
+      'processor class' => 'TMGMTFileXLIFFMaskHTMLProcessor',
+    ),
+  );
+}
+
+/**
  * Alter file format plugins provided by other modules.
  *
  * @see hook_tmgmt_file_format_plugin_info()
