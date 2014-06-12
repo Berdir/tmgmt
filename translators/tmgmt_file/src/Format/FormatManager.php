28614415f2eb28348ba3220af047ea269675ccc0
Sascha Grossenbacher <saschagros@gmail.com>
2014-06-12 16:27:49 +0200

    Remove LanguageManager from plugin managers.

--- translators/tmgmt_file/src/Format/FormatManager.php
+++ translators/tmgmt_file/src/Format/FormatManager.php
@@ -9,7 +9,6 @@ namespace Drupal\tmgmt_file\Format;
 
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Language\LanguageManager;
 use Drupal\Core\Plugin\DefaultPluginManager;
 
 /**
@@ -36,15 +35,13 @@ class FormatManager extends DefaultPluginManager {
    *   keyed by the corresponding namespace to look for plugin implementations.
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
    *   Cache backend instance to use.
-   * @param \Drupal\Core\Language\LanguageManager $language_manager
-   *   The language manager.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler to invoke the alter hook with.
    */
-  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
     parent::__construct('Plugin/tmgmt_file/Format', $namespaces, $module_handler, 'Drupal\tmgmt_file\Annotation\FormatPlugin');
     $this->alterInfo('tmgmt_file_format_plugin_info');
-    $this->setCacheBackend($cache_backend, $language_manager, 'tmgmt_file_format_plugin');
+    $this->setCacheBackend($cache_backend, 'tmgmt_file_format_plugin');
   }
 
   /**
