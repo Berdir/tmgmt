3d0ca8ff13cdbd9794819c2f13580663491321ce
git <git@3113479.no-reply.drupal.org>
2015-03-03 14:20:34 +0100

    Issue #2409641 by sasanikolic, Arla: Complete config entity source, add the test back

--- sources/tmgmt_config/src/Plugin/tmgmt/Source/ConfigEntitySource.php
+++ sources/tmgmt_config/src/Plugin/tmgmt/Source/ConfigEntitySource.php
@@ -7,10 +7,13 @@
 
 namespace Drupal\tmgmt_config\Plugin\tmgmt\Source;
 
-use Drupal\Core\Config\Schema\Element;
+use Drupal\config_translation\Form\ConfigTranslationFormBase;
+use Drupal\Core\Config\Schema\Mapping;
+use Drupal\Core\Config\Schema\Sequence;
+use Drupal\tmgmt\JobItemInterface;
 use Drupal\tmgmt\SourcePluginBase;
-use Drupal\tmgmt\Entity\JobItem;
 use Drupal\tmgmt\TMGMTException;
+use Drupal\Core\Render\Element;
 
 /**
  * Content entity source plugin controller.
@@ -61,6 +64,62 @@ class ConfigEntitySource extends SourcePluginBase {
    */
   public function saveTranslation(JobItemInterface $job_item) {
 
+    $entity = entity_load($job_item->getItemType(), $job_item->getItemId());
+    if (!$entity) {
+      throw new TMGMTException(t('Unable to load entity %type with id %id', array('%type' => $job_item->getItemType(), $job_item->getItemId())));
+    }
+    /* @var \Drupal\config_translation\ConfigMapperInterface $config_mapper */
+    $config_mapper = \Drupal::service('plugin.manager.config_translation.mapper')->createInstance($job_item->getItemType());
+    $config_mapper->setEntity($entity);
+
+    $data = $job_item->getData();
+
+    foreach ($config_mapper->getConfigNames() as $name) {
+      $schema = \Drupal::service('config.typed')->get($name);
+
+      // Set configuration values based on form submission and source values.
+      $base_config = \Drupal::configFactory()->getEditable($name);
+      $config_translation = \Drupal::languageManager()->getLanguageConfigOverride($job_item->getJob()->getTargetLangcode(), $name);
+
+      $element = ConfigTranslationFormBase::createFormElement($schema);
+      $element->setConfig($base_config, $config_translation, $this->convertToTranslation($data));
+
+      // If no overrides, delete language specific configuration file.
+      $saved_config = $config_translation->get();
+      if (empty($saved_config)) {
+        $config_translation->delete();
+      }
+      else {
+        $config_translation->save();
+      }
+    }
+
+  }
+
+  /**
+   * Converts a translated data structure. We convert it.
+   *
+   * @param array $data
+   *   The translated data structure.
+   *
+   * @return array
+   *   Returns a translation array as expected by
+   *   \Drupal\config_translation\FormElement\ElementInterface::setConfig().
+   *
+   */
+  public function convertToTranslation($data) {
+    $children = Element::children($data);
+    if ($children) {
+      $translation = array();
+      foreach ($children as $name) {
+        $property_data = $data[$name];
+        $translation[$name] = $this->convertToTranslation($property_data);
+      }
+      return $translation;
+    }
+    elseif (isset($data['#translation']['#text'])) {
+      return $data['#translation']['#text'];
+    }
   }
 
   /**
@@ -116,7 +175,7 @@ class ConfigEntitySource extends SourcePluginBase {
       $element_key = implode('.', array_filter(array($base_key, $key)));
       $definition = $element->getDataDefinition();
         // + array('label' => t('N/A'));
-      if ($element instanceof Element) {
+      if ($element instanceof Mapping || $element instanceof Sequence) {
         // Build sub-structure and include it with a wrapper in the form
         // if there are any translatable elements there.
         $sub_data = $this->extractTranslatables($element, $config_data[$key], $element_key);
@@ -137,6 +196,5 @@ class ConfigEntitySource extends SourcePluginBase {
     }
     return $data;
   }
-
 
 }
---
+++ sources/tmgmt_config/src/Tests/ConfigEntitySourceUnitTest.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\tmgmt_config\Tests\ConfigEntitySourceUnitTest.
+ */
+
+namespace Drupal\tmgmt_config\Tests;
+
+use Drupal\language\Entity\ConfigurableLanguage;
+use Drupal\node\Entity\NodeType;
+use Drupal\system\Tests\Entity\EntityUnitTestBase;
+use Drupal\views\Entity\View;
+
+/**
+ * Unit tests for exporting translatable data from config entities and saving it back.
+ *
+ * @group tmgmt
+ */
+class ConfigEntitySourceUnitTest extends EntityUnitTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('tmgmt', 'tmgmt_config', 'tmgmt_test', 'node', 'entity', 'filter', 'language', 'config_translation', 'locale', 'menu_link', 'views', 'views_ui');
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    // Add the languages.
+    $this->installConfig(['language']);
+    ConfigurableLanguage::createFromLangcode('de')->save();
+
+    $this->installEntitySchema('tmgmt_job');
+    $this->installEntitySchema('tmgmt_job_item');
+    $this->installEntitySchema('tmgmt_message');
+    $this->installSchema('system', array('router'));
+    $this->installSchema('locale', array('locales_location'));
+
+    \Drupal::service('router.builder')->rebuild();
+
+    tmgmt_translator_auto_create(\Drupal::service('plugin.manager.tmgmt.translator')->getDefinition('test_translator'));
+  }
+
+  /**
+   * Tests the node type config entity.
+   */
+  public function testNodeType() {
+    // Create an english test entity.
+    $node_type = NodeType::create(array(
+      'type' => 'test',
+      'name' => 'Node type name',
+      'description' => 'Node type description',
+      'title_label' => 'Title label',
+      'langcode' => 'en',
+    ));
+    $node_type->save();
+
+    $job = tmgmt_job_create('en', 'de');
+    $job->translator = 'test_translator';
+    $job->save();
+    $job_item = tmgmt_job_item_create('config', 'node_type', $node_type->id(), array('tjid' => $job->id()));
+    $job_item->save();
+
+    $source_plugin = $this->container->get('plugin.manager.tmgmt.source')->createInstance('config');
+    $data = $source_plugin->getData($job_item);
+
+    // Test the name property.
+    $this->assertEqual($data['name']['#label'], 'Name');
+    $this->assertEqual($data['name']['#text'], $node_type->label());
+    $this->assertEqual($data['name']['#translate'], TRUE);
+    $this->assertEqual($data['description']['#label'], 'Description');
+    $this->assertEqual($data['description']['#text'], $node_type->getDescription());
+    $this->assertEqual($data['description']['#translate'], TRUE);
+
+    // Test item types.
+    $this->assertEqual($source_plugin->getItemTypes()['node_type'], t('Content type'));
+
+    // Now request a translation and save it back.
+    $job->requestTranslation();
+    $items = $job->getItems();
+    $item = reset($items);
+    $item->acceptTranslation();
+    $data = $item->getData();
+
+    // Check that the translations were saved correctly.
+    $language_manager = \Drupal::languageManager();
+    $language_manager->setConfigOverrideLanguage($language_manager->getLanguage('de'));
+    $node_type = entity_load('node_type', $node_type->id());
+
+    $this->assertEqual($node_type->label(), $data['name']['#translation']['#text']);
+    $this->assertEqual($node_type->getDescription(), $data['description']['#translation']['#text']);
+  }
+
+  /**
+   * Tests the view config entity.
+   */
+  public function testView() {
+    $this->installConfig(['system', 'tmgmt']);
+    $job = tmgmt_job_create('en', 'de');
+    $job->translator = 'test_translator';
+    $job->save();
+    $job_item = tmgmt_job_item_create('config', 'view', 'tmgmt_job_overview', array('tjid' => $job->id()));
+    $job_item->save();
+
+    $source_plugin = $this->container->get('plugin.manager.tmgmt.source')->createInstance('config');
+    $data = $source_plugin->getData($job_item);
+
+    // Test the name property.
+    $this->assertEqual($data['label']['#label'], 'Label');
+    $this->assertEqual($data['label']['#text'], $job_item->label());
+    $this->assertEqual($data['label']['#translate'], TRUE);
+    $this->assertEqual($data['description']['#label'], 'Administrative description');
+    $this->assertEqual($data['description']['#text'], 'Gives a bulk operation overview of translation jobs in the system.');
+    $this->assertEqual($data['description']['#translate'], TRUE);
+    $this->assertEqual($data['display']['default']['display_title']['#text'], 'Master');
+    $this->assertEqual($data['display']['default']['display_options']['exposed_form']['options']['submit_button']['#label'], 'Submit button text');
+    $this->assertEqual($data['display']['default']['display_options']['pager']['options']['expose']['items_per_page_label']['#label'], 'Items per page label');
+
+    // Test item types.
+    $this->assertEqual($source_plugin->getItemTypes()['view'], t('View'));
+
+    // Now request a translation and save it back.
+    $job->requestTranslation();
+    $items = $job->getItems();
+    $item = reset($items);
+    $item->acceptTranslation();
+    $data = $item->getData();
+
+    // Check that the translations were saved correctly.
+    $language_manager = \Drupal::languageManager();
+    $language_manager->setConfigOverrideLanguage($language_manager->getLanguage('de'));
+    $view = View::load('tmgmt_job_overview');
+
+    $this->assertEqual($view->label(), $data['label']['#translation']['#text']);
+    $this->assertEqual($view->get('description'), $data['description']['#translation']['#text']);
+
+    $display = $view->get('display');
+    $this->assertEqual($display['default']['display_options']['title'], $data['label']['#translation']['#text']);
+    $this->assertEqual($display['default']['display_options']['exposed_form']['options']['submit_button'], $data['display']['default']['display_options']['exposed_form']['options']['submit_button']['#translation']['#text']);
+  }
+
+}
