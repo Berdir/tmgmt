9816168553d88ea8bf9a7aa52840d5885457f38f
git <git@3113479.no-reply.drupal.org>
2015-03-18 15:12:58 +0100

    Issue #2450259 by sasanikolic: Properly show translations status and help in the table

--- sources/content/src/ContentEntitySourcePluginUi.php
+++ sources/content/src/ContentEntitySourcePluginUi.php
@@ -285,7 +285,11 @@ class ContentEntitySourcePluginUi extends SourcePluginUiBase {
   public function overviewForm(array $form, FormStateInterface $form_state, $type) {
     $form += $this->overviewSearchFormPart($form, $form_state, $type);
 
-    $form['#attached']['library'][] = 'tmgmt/admin';
+     $form['#attached']['library'][] = 'tmgmt/admin';
+    // @todo: This should be added in tmgmt_color_legend().
+    if (\Drupal::theme()->getActiveTheme()->getName() == 'seven') {
+      $form['#attached']['library'][] = 'tmgmt/admin.seven';
+    }
 
     $form['items'] = array(
       '#type' => 'tableselect',
