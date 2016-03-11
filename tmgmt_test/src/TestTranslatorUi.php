9d1d6094d1ae658a38b1ea4d5313b37229b3b0f7
mbovan <mbovan@3149657.no-reply.drupal.org>
2016-03-11 17:35:36 +0100

    Issue #2685549 by mbovan: Add default settings for tmgmt test translator

--- tmgmt_test/src/TestTranslatorUi.php
+++ tmgmt_test/src/TestTranslatorUi.php
@@ -23,10 +23,13 @@ class TestTranslatorUi extends TranslatorPluginUiBase {
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
 
+    /** @var \Drupal\tmgmt\TranslatorInterface $test_translator */
+    $test_translator = $form_state->getFormObject()->getEntity();
+
     $form['expose_settings'] = array(
       '#type' => 'checkbox',
       '#title' => t('Display settings'),
-      '#default_value' => TRUE,
+      '#default_value' => $test_translator->getSetting('expose_settings'),
     );
 
     $form['action'] = array(
@@ -40,6 +43,7 @@ class TestTranslatorUi extends TranslatorPluginUiBase {
         'not_available' => t('Not available'),
         'not_translatable' => t('Not translatable'),
       ),
+      '#default_value' => $test_translator->getSetting('action'),
     );
     return $form;
   }
